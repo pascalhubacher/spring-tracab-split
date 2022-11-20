121ef02f3ca19aef00b148e42ac19d796da84dbc
Pascal Hubacher <pascal.hubacher@outlook.com>
2022-11-20 21:42:41 +0100

    use playerball schema

--- src/main/java/com/mas2022datascience/springtracabsplit/processor/KafkaStreamsRunnerDSL.java
+++ src/main/java/com/mas2022datascience/springtracabsplit/processor/KafkaStreamsRunnerDSL.java
@@ -2,7 +2,11 @@ package com.mas2022datascience.springtracabsplit.processor;
 
 import com.mas2022datascience.avro.v1.Frame;
 import com.mas2022datascience.avro.v1.Object;
+import com.mas2022datascience.avro.v1.PlayerBall;
 import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
+import java.time.Instant;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
@@ -39,11 +43,24 @@ public class KafkaStreamsRunnerDSL {
     KStream<String, Frame> stream = kStreamBuilder.stream(topicIn,
         Consumed.with(Serdes.String(), frameSerde));
 
-    KStream<String, Object> playerStream = stream.flatMap(
+    KStream<String, PlayerBall> playerStream = stream.flatMap(
       (key, value) -> {
-        List<KeyValue<String, Object>> result = new LinkedList<>();
+        List<KeyValue<String, PlayerBall>> result = new LinkedList<>();
         for ( Object valueObject : value.getObjects() ) {
-          result.add(KeyValue.pair(value.getMatch().getId()+"-"+valueObject.getId(), valueObject));
+          result.add(KeyValue.pair(
+              value.getMatch().getId()+"-"+valueObject.getId(),
+              PlayerBall
+                  .newBuilder()
+                  .setId(valueObject.getId())
+                  .setTs(Instant.ofEpochMilli(utcString2epocMs(value.getUtc())))
+                  .setX(valueObject.getX())
+                  .setY(valueObject.getY())
+                  .setZ(valueObject.getZ())
+                  .setVelocity(valueObject.getVelocity())
+                  .setAccelleration(valueObject.getAccelleration())
+                  .build()
+              )
+          );
         }
         return result;
       }
@@ -52,6 +69,18 @@ public class KafkaStreamsRunnerDSL {
 
     return stream;
 
+  }
+
+  /**
+   * Converts the utc string of type "yyyy-MM-dd'T'HH:mm:ss.SSS" to epoc time in milliseconds.
+   * @param utcString of type String of format 'yyyy-MM-dd'T'HH:mm:ss.SSS'
+   * @return epoc time in milliseconds
+   */
+  private static long utcString2epocMs(String utcString) {
+    DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS")
+        .withZone(ZoneOffset.UTC);
+
+    return Instant.from(fmt.parse(utcString)).toEpochMilli();
   }
 }
 
